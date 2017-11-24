479943198b8692345e3d75893edc73af2b3c9832
Eric Smith <ericsmith@Erics-MacBook-Pro.local>
2017-11-24 13:33:39 +1300

    Init basic config entity

---
+++ src/Entity/FeedsTamper.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\feeds_tamper\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+
+/**
+ * Defines the Feeds tamper entity.
+ *
+ * @ConfigEntityType(
+ *   id = "feeds_tamper",
+ *   label = @Translation("Feeds tamper"),
+ *   handlers = {
+ *     "form" = {
+ *       "add" = "Drupal\feeds_tamper\Form\FeedsTamperForm",
+ *       "edit" = "Drupal\feeds_tamper\Form\FeedsTamperForm",
+ *       "delete" = "Drupal\feeds_tamper\Form\FeedsTamperDeleteForm"
+ *     },
+ *     "list_builder" = "Drupal\feeds_tamper\FeedsTamperListBuilder",
+ *   },
+ *   config_prefix = "feeds_tamper",
+ *   admin_permission = "administer site configuration",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *   },
+ *   links = {
+ *     "canonical" = "/admin/structure/feeds_tamper/{feeds_tamper}",
+ *     "add-form" = "/admin/structure/feeds_tamper/add",
+ *     "edit-form" = "/admin/structure/feeds_tamper/{feeds_tamper}/edit",
+ *     "delete-form" = "/admin/structure/feeds_tamper/{feeds_tamper}/delete",
+ *     "collection" = "/admin/structure/feeds_tamper"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "targetFeedType",
+ *     "tampers",
+ *   }
+ * )
+ */
+class FeedsTamper extends ConfigEntityBase implements FeedsTamperInterface {
+
+  /**
+   * The Feeds tamper ID.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * The feed type id.
+   *
+   * @var string
+   */
+  protected $targetFeedType;
+
+  /**
+   * List of tampers to apply.
+   *
+   * @var array
+   */
+  protected $tampers = [];
+
+  public function getTargetFeedType() {
+    return $this->targetFeedType;
+  }
+
+}
---
+++ src/Entity/FeedsTamperInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\feeds_tamper\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface for defining Feeds tamper entities.
+ */
+interface FeedsTamperInterface extends ConfigEntityInterface {
+
+  // Add get/set methods for your configuration properties here.
+}
